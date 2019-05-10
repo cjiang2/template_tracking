e0c279136e0e870418fc04b27559e08fb6f2a7a3
zonetrooper32 <zone3692@hotmail.com>
2019-05-10 22:18:01 +0800

    Added inverse operation in homography test case.

--- experiments/test_cases/test_homography.py
+++ experiments/test_cases/test_homography.py
@@ -20,6 +20,11 @@ ground_truths = read_tracking_data(ROOT_DIR+'/datasets/TMT/nl_cereal_s1.txt')
 im1 = cv2.imread(ROOT_DIR+'/datasets/TMT/nl_cereal_s1/frame00001.jpg', 0)
 corners_1 = ground_truths[0]
 
+vis1 = draw_region(im1, corners_1)
+plt.imshow(vis1)
+plt.title('Frame 1')
+plt.show()
+
 # Sample patches, with resizing inwards, using least square
 # Template patch, direct resizing
 patch_1 = sample_region(im1, corners_1, region_shape=(128, 128))
@@ -31,15 +36,16 @@ print()
 
 # One synthetic motion
 # ------------------------------
+np.random.seed(0)
 current_warp = square_to_corners_warp(corners_1)
 motion_param = (0.03, 0.02)
 sigma_d, sigma_t = motion_param
 H = random_homography(sigma_d, sigma_t)
-current_warp = np.matmul(current_warp, H)   # Inverse composition
-
+current_warp = np.matmul(current_warp, np.linalg.inv(H))  # Inverse composition
 search_corners = np.round(apply_homography(current_warp, _SQUARE)).astype(int)
 print('original corners:', corners_1)
 print('search corners:', search_corners)
+
 vis1 = draw_region(vis1, search_corners, (0, 0, 255))
 plt.imshow(vis1)
 plt.title('Frame 1(corners)')
