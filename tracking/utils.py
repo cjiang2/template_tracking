17ae7f28eb8f160449e4024b90ac4a28d430314d
Zone Trooper <zone3692@hotmail.com>
2019-08-14 15:47:28 -0600

    Update utils.py

--- tracking/utils.py
+++ tracking/utils.py
@@ -14,7 +14,7 @@ def sample_region(img,
                   corners, 
                   region_shape=None,
                   method=0, 
-                  interpolation=cv2.INTER_LINEAR,
+                  interpolation=cv2.INTER_NEAREST,
                   Np=None):
     """Sample corners region as Bird-eye view patch. Left top point of the 
     rect corners is defined as the origin coordinate (0, 0).
@@ -81,29 +81,6 @@ def polys_to_mask(img,
     mask = np.zeros(img.shape[:2], dtype=np.uint8)
     cv2.fillPoly(mask, [polys], 255)
     return mask
-
-def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
- 
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
- 
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
- 
-	# return the ordered coordinates
-	return rect
 
 def homogenize(corners):
     """Transform points (n, m) into their homogeneous coordinate 
